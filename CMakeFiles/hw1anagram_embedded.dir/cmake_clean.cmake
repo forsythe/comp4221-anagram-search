4d80a9e3cda67cbf9d80fab3a96cd0a25e4343c7
SU Heng <hsuab@csl2wk20.cse.ust.hk>
2018-03-25 19:10:47 +0800

    compiled for lab machine

---
+++ CMakeFiles/hw1anagram_embedded.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hw1anagram_embedded.dir/main.cpp.o"
+  "hw1anagram_embedded.pdb"
+  "hw1anagram_embedded"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hw1anagram_embedded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
